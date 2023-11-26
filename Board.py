039df0fd1ca9d38685614aa6017a86f0599eb4c7
dasmeko <dashadadaday@gmail.com>
2023-11-26 18:30:35 +0200

    Created class board, init, move

--- Board.py
+++ Board.py
@@ -6,3 +6,30 @@ class Tile:
         self.y = (2 * (n // ROWS) + 1) * TILE_SIZE // 2
         self.value = 0
         self.index = n
+
+class Board:
+    def __init__(self):
+        self.tiles = list(Tile(i) for i in range(ROWS**2))
+        self.fullness = 0
+        
+    def move(self, player, pos):
+        min_dist = TILE_SIZE
+        index = 0
+        x, y = pos
+        
+        for tile in self.tiles:
+            dist = math.sqrt((x - tile.x)**2 + (y - tile.y)**2)
+            
+            if dist < min_dist:
+                min_dist = dist
+                index = tile.index
+                
+        if not self.tiles[index].value:
+            self.fullness += 1
+            self.tiles[index].value = player + 1
+            moved = True
+        else:
+            moved = False
+        
+        return moved
+        
